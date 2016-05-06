aca03c9d9247a1f8bf768a32ff79c531d5d33f1f
sam961124 <sam961124@gmail.com>
2016-05-06 17:22:54 +0800

    index update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('landing/index', { title: 'Express' });
+  res.render('landing/index');
+});
+router.get('/reg', function(req, res, next) {
+  res.render('landing/register');
 });
 
 module.exports = router;
