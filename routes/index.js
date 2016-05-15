b2f1fbe1dbc54e0b06bc654a2c0b312c1b2ae0be
sam961124 <sam961124@gmail.com>
2016-05-15 22:15:53 +0800

    case

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,11 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('landing/index');
 });
+router.get('/mycase', function(req, res, next) {
+  res.render('landing/mycase');
+});
+router.get('/mywork', function(req, res, next) {
+  res.render('landing/mywork');
+});
 
 module.exports = router;
