5cb5dfc5e8e0ceb9aaca67abbcd5f39dcbb64756
Brian <brian8311@gmail.com>
2016-05-16 12:41:07 +0800

    profile

--- routes/index.js
+++ routes/index.js
@@ -10,5 +10,12 @@ router.get('/', function(req, res, next) {
 router.get('/profile', function(req, res, next) {
   res.render('landing/profile');
 });
-
+/* Get mycase page */
+router.get('/mycase', function(req, res, next) {
+  res.render('landing/mycase');
+});
+ /* Get mywork page*/
+ router.get('/mywork', function(req, res, next) {
+   res.render('landing/mywork');
+ });
 module.exports = router;
