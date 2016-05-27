4c1c8154aa8d29649061900bda7a0f996ea9c1a0
Brian <brian8311@gmail.com>
2016-05-27 17:00:01 +0800

    profile

---
+++ public/javascript/profile.js
@@ -0,0 +1,10 @@
+four.controller('resumeController', function($scope){
+    $scope.resumes = [
+      {
+        "education" : "國立台灣大學 資訊管理學系(在學中)",
+        "workExperience" : "陳立數學輔導老師",
+        "achievement" : "在大二那年參加政大生活創客黑客松比賽在三天內架好了網站還有摸了一些java然後寫出一個android版本的app獲得評審的肯定。那幾天幾乎是挑戰自己的極限沒日沒夜討論跟努力趕出作品。",
+        "worklink" : "詳細履歷"
+      }
+    ];
+});
