05326db13bab80e71998d3560d15efdb3017baeb
Brian <brian8311@gmail.com>
2016-05-31 17:36:15 +0800

    nochange

--- public/javascript/event.js
+++ public/javascript/event.js
@@ -1,5 +1,36 @@
 var four = angular.module('four', ['ngRoute']);
 
+//directive
+four.directive('fileModel', ['$parse', function($parse){
+	return {
+		restrict: 'A',
+		link: function(scope, element, attrs){
+			var model = $parse(attrs.fileModel);
+			var modelSetter = model.assign;
+
+			element.bind('change', function(){
+				scope.$apply(function(){
+					modelSetter(scope, element[0].files[0]);
+				})
+			})
+		}
+	}
+}])
+
+//service
+four.service('multipartForm', ['$http', function($http){
+	this.post = function(uploadUrl, data){
+		var fd = new FormData();
+		for(var key in data)
+			fd.append(key, data[key]);
+		$http.post(uploadUrl, fd, {
+			transformRequest: angular.indentity,
+			headers: { 'Content-Type': undefined }
+		});
+	}
+}])
+
+//controller
 four.controller("LoginController", function($scope, $window){
     $scope.message = "請登入";
     $scope.fb_login = function(){
@@ -8,10 +39,9 @@ four.controller("LoginController", function($scope, $window){
     $scope.gp_login = function(){
             $scope.message = "登入中"
     };
-
 });
 
-four.controller('caseFormController', ['$scope', function($scope) {
+four.controller('caseFormController', ['$scope', 'multipartForm', function($scope, multipartForm) {
      $scope.case = {
          name: "我的案子",
          money: 20000,
@@ -21,6 +51,11 @@ four.controller('caseFormController', ['$scope', function($scope) {
          applicant: 0,
          location: "地區",
          detail: null
+    };
+    $scope.submit = function() {
+        // alert($scope.case.location);
+        var uploadUrl = '/create_case';
+		multipartForm.post(uploadUrl, $scope.case);
     }
 }]);
 
@@ -37,29 +72,6 @@ four.controller('SelectHeaderController', function($scope, $element) {
       });
 });
 
-four.controller('InputDropdownController', [
-    '$scope',
-    '$q',
-    function($scope, $q) {
-        var self = this;
-        self.selectedDropdownItem = null;
-        self.defaultDropdownStrings = ['不限', '基隆市', '台北市', '新北市', '桃園市', '新竹市', '新竹縣', '苗栗縣', '台中市', '彰化縣', '雲林縣', '嘉義市', '嘉義縣', '台南市', '台南縣', '高雄市', '高雄縣', '屏東縣', '宜蘭縣', '花蓮縣', '台東縣', '澎湖縣', '其他'];
-        self.filterStringList = function(userInput) {
-            var filter = $q.defer();
-            var normalisedInput = userInput.toLowerCase();
-
-            var filteredArray =     self.defaultDropdownStrings.filter(function(country) {
-            return country.toLowerCase().indexOf(normalisedInput) === 0;
-            });
-
-            filter.resolve(filteredArray);
-            return filter.promise;
-        };
-        self.itemStringSelected = function(item) {
-            console.log('Handle item string selected in controller:', item);
-        };
-}]);
-
 four.controller('AppCtrl', function($scope) {
     $scope.endDate = new Date();
 
@@ -73,6 +85,7 @@ four.controller('AppCtrl', function($scope) {
     $scope.endDate.getMonth() + 2,
     $scope.endDate.getDate());
 });
+
 
 four.controller('mycaseController', ['$scope', function($scope) {
      $scope.cases = [
