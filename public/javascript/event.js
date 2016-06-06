0d78fbb91be409fcd8acb2beec666e6897df0dba
sam961124 <sam961124@gmail.com>
2016-06-06 19:51:27 +0800

    fix bug

--- public/javascript/event.js
+++ public/javascript/event.js
@@ -37,7 +37,7 @@ four.service('multipartForm', ['$http', '$window', function($http, $window){
 
 //controller
 
-four.controller('caseFormController', ['$scope', 'multipartForm', '$window', function($scope, multipartForm ,$window) {
+four.controller('caseFormController', function($element, $scope, multipartForm ,$window) {
      $scope.case = {
          name: "我的案子",
          money: 20000,
@@ -64,6 +64,18 @@ four.controller('caseFormController', ['$scope', 'multipartForm', '$window', fun
 	$element.find('input').on('keydown', function(ev) {
 		ev.stopPropagation();
 	});
+	$scope.cases = [
+     {name: '那些年我們一起追的女孩', applicant: 20, date: '2016.5.10', location: '台北市', money: 20000, pic_url: '/image/thatyear.jpg', status: 'finding', status_word: '找人中'},
+     {name: '等一個人咖啡', applicant: 5, date: '2016.5.20', location: '台中市', money: 50000, pic_url: '/image/waiting.jpg', status: 'found', status_word: '找人成功'},
+     {name: '我的少女時代', applicant: 0, date: '2016.5.15', location: '新北市', money: 25000, pic_url: '/image/ourtime.jpg', status: 'finding', status_word: '找人中'},
+     {name: '那些年我們一起追的女孩', applicant: 20, date: '2016.5.10', location: '台北市', money: 20000, pic_url: '/image/thatyear.jpg', status: 'found', status_word: '找人成功'},
+     {name: '那些年我們一起追的女孩', applicant: 20, date: '2016.5.10', location: '台北市', money: 20000, pic_url: '/image/thatyear.jpg', status: 'found', status_word: '找人成功'},
+     {name: '那些年我們一起追的女孩', applicant: 20, date: '2016.5.10', location: '台北市', money: 20000, pic_url: '/image/thatyear.jpg', status: 'finding', status_word: '找人中'},
+     {name: '那些年我們一起追的女孩', applicant: 20, date: '2016.5.10', location: '台北市', money: 20000, pic_url: '/image/thatyear.jpg', status: 'finding', status_word: '找人中'},
+     {name: '那些年我們一起追的女孩', applicant: 20, date: '2016.5.10', location: '台北市', money: 20000, pic_url: '/image/thatyear.jpg', status: 'finished', status_word: '已完工'},
+     {name: '那些年我們一起追的女孩', applicant: 20, date: '2016.5.10', location: '台北市', money: 20000, pic_url: '/image/thatyear.jpg', status: 'finished', status_word: '已完工'},
+     {name: '那些年我們一起追的女孩', applicant: 20, date: '2016.5.10', location: '台北市', money: 20000, pic_url: '/image/thatyear.jpg', status: 'finished', status_word: '已完工'}
+    ];
 });
 
 four.controller('AppCtrl', function($scope) {
